bbb8f90d0931eb41dd6b3e66c3b46c90c3e813b6
thebjorn <bp@datakortet.no>
2024-04-10 16:39:59 +0200

    works in dev

--- apps/sweb/src/routes/+page.server.js
+++ apps/sweb/src/routes/+page.server.js
@@ -1,3 +1,13 @@
 import {PrismaClient} from 'database'
 
 const client = new PrismaClient()
+
+export async function load({locals}) {
+    return {
+        user: await client.person.findUnique({
+            where: {
+                id: 'cluprcuev0000sdh4z5bumfhn'
+            }
+        })
+    }
+}
